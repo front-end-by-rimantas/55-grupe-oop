189b1885288ac9e173cc0d23dab907bba8b536bd
Rimantas <belauzas@gmail.com>
2025-04-15 09:05:33 +0300

    Dog

---
+++ js/Dog.js
@@ -0,0 +1,14 @@
+export class Dog {
+    constructor(name, furColor) {
+        this.name = name;
+        this.furColor = furColor;
+    }
+
+    hi() {
+        return `Hi, my name is ${this.name}.`;
+    }
+
+    voice() {
+        return `${this.name}: au au 🐕🐕!`;
+    }
+}
